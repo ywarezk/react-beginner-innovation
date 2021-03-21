f5ee055ca3a062dae433027661e67b8d306ad36b
ywarezk <ywarezk@gmail.com>
2021-03-21 16:29:15 +0200

    added myu solution

---
+++ 02-jsx/ex-sol/README.md
@@ -0,0 +1,37 @@
+# Login Excercise
+
+In this excercise you will create a login web application.
+The application will consist of the following components:
+
+## App
+
+This will be the root of your component tree.  
+this component will place all the other components
+
+## Header
+
+This component will be placed at the top of the screen.  
+On the left side of the component place a logo image.  
+On the right side of the component, place a navigation bar with links to:
+
+- home page
+- about page
+- todo list page
+
+## Footer
+
+This component should be placed at the bottom of the screen with an ltd text
+
+
+## Login
+
+This component will be placed at the center of the screen.  
+It will hold a form with an email and a password field and a submit button.  
+
+Try and design your app using bootstrap which is already added to the `index.html`
+
+## Run in browser
+
+```
+npx http-server
+```
--- 02-jsx/ex-sol/todo.js
+++ 02-jsx/ex-sol/todo.js
@@ -21,8 +21,30 @@ function Header() {
 }
 
 function Login() {
+	const isLoading = false;
+	
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const response = await fetch(
+			'https://academeez-login-ex.herokuapp.com/api/users/login',
+			{
+				method: 'POST',
+				body: JSON.stringify({
+					email: 'yariv@nerdeez.com',
+					password: '12345678'
+				}),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		)
+		const data = await response.json();
+		console.log(data);
+		
+	}
+	
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className="mb-3">
 				<label className="form-label">Email address</label>
 				<input type="email" className="form-control" />	
@@ -31,7 +53,14 @@ function Login() {
 				<label>Password</label>
 				<input type="password" className="form-control" />
 			</div>
-			<button type="submit" className="btn btn-primary">Submit</button>
+			<button 
+				disabled={isLoading}
+				type="submit" 
+				className="btn btn-primary"
+			>Submit</button>
+			{
+				isLoading && <h1>Loading...</h1>
+			}
 		</form>
 	)
 }
